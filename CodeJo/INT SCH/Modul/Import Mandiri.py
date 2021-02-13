c6cf484eeda0b9def533e17af08070888e29fdf0
johanpramudito <ajohanpramudito@gmail.com>
2021-02-13 09:42:15 +0700

    Pelajaran Modul

---
+++ CodeJo/INT SCH/Modul/Import Mandiri.py
@@ -0,0 +1,13 @@
+#Script program induk
+#import modul
+import fibo
+
+#pemanggilan fungsi pertama
+print(fibo.fib(100))
+
+#pemanggilan fungsi ke dua
+print(fibo.fib2(100))
+
+#pemanggilan fungsi dengan nama lokal
+fib = fibo.fib
+print(fib(100))
